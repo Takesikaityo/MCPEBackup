701b2025781afa9f280501747908f2f6027787fc
たけしかいちょー(takesi) <piggtomop@gmail.com>
2017-04-30 01:26:51 +0900

    added

--- app/src/main/java/jp/ueda/mcpebackup/MainActivity.java
+++ app/src/main/java/jp/ueda/mcpebackup/MainActivity.java
@@ -1,6 +1,7 @@
 package jp.ueda.mcpebackup;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
@@ -12,6 +13,7 @@ import android.os.Environment;
 import android.support.customtabs.CustomTabsIntent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -36,6 +38,7 @@ import java.net.URL;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -52,12 +55,76 @@ public class MainActivity extends AppCompatActivity{
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+                // permissionが許可されていません
+                if (shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS)) {
+                    // 許可ダイアログで今後表示しないにチェックされていない場合
+                }
+                // permissionを許可してほしい理由の表示など
+                // 許可ダイアログの表示
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTSはアプリ内で独自定義したrequestCodeの値
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                //return;
+            }
+        }
+
+            AlertDialog.Builder alertDialog=new AlertDialog.Builder(this);
+            alertDialog.setTitle(R.string.terms);//set title
+            alertDialog.setMessage(R.string.termsof);//set content
+            alertDialog.setIcon(R.mipmap.ic_launcher);//set icon
+            alertDialog.setCancelable(false);
+            alertDialog.setPositiveButton("OK",new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+
+        String sdPath = Environment.getExternalStorageDirectory() + "/MCPEBackups/";
+        files = new File(sdPath).listFiles();
+        if (files != null) {
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].isFile() && files[i].getName().endsWith(".apk")) {
+                    songList.add(files[i].getName());
+                }
+            }
+
+            lv = (ListView) findViewById(R.id.listview);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, songList);
+            lv.setAdapter(adapter);
+
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    ListView listView = (ListView) parent;
+                    String item = (String) listView.getItemAtPosition(position);
+                    try {
+                        Toast.makeText(getApplicationContext(),R.string.show,Toast.LENGTH_LONG).show();
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Uri uri = Uri.fromParts("package", "com.mojang.minecraftpe", null);
+                    Intent intent = new Intent(Intent.ACTION_DELETE, uri);
+                    startActivity(intent);
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    String fileName = Environment.getExternalStorageDirectory() + "/MCPEBackups/" + item;
+                    Intent intent1 = new Intent(Intent.ACTION_VIEW);
+                    intent1.setDataAndType(Uri.fromFile(new File(fileName)), "application/vnd.android.package-archive");
+                    startActivity(intent1);
+                }
+            });
+
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         if (bundle != null) {
             String type = bundle.getString("type");
-            assert type != null;
-            if (type.equals("backup")) {
+            if (Objects.equals(type, "backup")) {
                 Log.d("[MCPEBackup]", "Starts MCPE APK Backup");
                 PackageManager pm = this.getPackageManager();
                 String versionName = "";
@@ -124,46 +191,6 @@ public class MainActivity extends AppCompatActivity{
         // build it by setting up all
         customTabsIntent = intentBuilder.build();
 
-
-        String sdPath = Environment.getExternalStorageDirectory() + "/MCPEBackups/";
-        files = new File(sdPath).listFiles();
-        if (files != null) {
-            for (int i = 0; i < files.length; i++) {
-                if (files[i].isFile() && files[i].getName().endsWith(".apk")) {
-                    songList.add(files[i].getName());
-                }
-            }
-
-            lv = (ListView) findViewById(R.id.listview);
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, songList);
-            lv.setAdapter(adapter);
-
-            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    ListView listView = (ListView) parent;
-                    String item = (String) listView.getItemAtPosition(position);
-                    try {
-                        Toast.makeText(getApplicationContext(),R.string.show,Toast.LENGTH_LONG).show();
-                        Thread.sleep(3000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    Uri uri = Uri.fromParts("package", "com.mojang.minecraftpe", null);
-                    Intent intent = new Intent(Intent.ACTION_DELETE, uri);
-                    startActivity(intent);
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    String fileName = Environment.getExternalStorageDirectory() + "/MCPEBackups/" + item;
-                    Intent intent1 = new Intent(Intent.ACTION_VIEW);
-                    intent1.setDataAndType(Uri.fromFile(new File(fileName)), "application/vnd.android.package-archive");
-                    startActivity(intent1);
-                }
-            });
-
             FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -174,20 +201,6 @@ public class MainActivity extends AppCompatActivity{
                     startActivity(intent);
                 }
             });
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-                    // permissionが許可されていません
-                    if (shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS)) {
-                        // 許可ダイアログで今後表示しないにチェックされていない場合
-                    }
-                    // permissionを許可してほしい理由の表示など
-                    // 許可ダイアログの表示
-                    // MY_PERMISSIONS_REQUEST_READ_CONTACTSはアプリ内で独自定義したrequestCodeの値
-                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-                    return;
-                }
-            }
         }
     }
 
@@ -201,7 +214,7 @@ public class MainActivity extends AppCompatActivity{
             c.connect();
 
             // SDカードの設定
-            String PATH = Environment.getExternalStorageDirectory() + "/MCPEBackup/tmp/";
+            String PATH = Environment.getExternalStorageDirectory() + "/MCPEBackups/tmp/";
             File file = new File(PATH);
             file.mkdirs();
 
@@ -258,12 +271,12 @@ public class MainActivity extends AppCompatActivity{
         if (id == R.id.about) {
             AlertDialog alertDialog = new AlertDialog.Builder(this).create();
             alertDialog.setTitle(getString(R.string.app_name) + BuildConfig.VERSION_NAME );
-            alertDialog.setIcon(R.mipmap.icon);
+            alertDialog.setIcon(R.mipmap.ic_launcher);
             alertDialog.setMessage(getString(R.string.author) + "\n\n" +
                     getString(R.string.email) + "\n\n" +
                     getString(R.string.github_link) +  "\n\n" +
                     getString(R.string.date) + "\n");
-            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "ok",
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
                     new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
